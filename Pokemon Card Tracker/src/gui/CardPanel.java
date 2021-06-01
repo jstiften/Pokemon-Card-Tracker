5844fa25cde95c2cfb9caeb499e025b90d434827
jstiften <jstiften@gmail.com>
2021-05-31 21:19:23 -0500

    Added Card name and radio buttons

--- Pokemon Card Tracker/src/gui/CardPanel.java
+++ Pokemon Card Tracker/src/gui/CardPanel.java
@@ -2,19 +2,49 @@ package gui;
 
 import java.awt.FlowLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
 public class CardPanel extends JPanel{
 
-	private JLabel test;
+	private JLabel cardName;
+	private JRadioButton obtained;
+	private JRadioButton notObtained;
 	
-	public CardPanel() {
+	public CardPanel(String cardName) {
 		setLayout(new FlowLayout());
 		
-		test = new JLabel("Test");
-		add(test);
+		this.cardName = new JLabel(cardName);
+		add(this.cardName);
+		
+		obtained = new JRadioButton("Have");
+		
+		notObtained = new JRadioButton("Do Not Have");
+		
+		ButtonGroup haveOrNot = new ButtonGroup();
+		haveOrNot.add(obtained);
+		haveOrNot.add(notObtained);
+		
+		add(obtained);
+		add(notObtained);
 	}
 	
+	public JLabel getCardName() {
+		return this.cardName;
+	}
+	
+	public void setCardName(String cardName) {
+		this.cardName.setText(cardName);
+	}
+	
+	public JRadioButton getObtained() {
+		return this.obtained;
+	}
+	
+	public JRadioButton getNotObtained() {
+		return this.notObtained;
+	}
 }
